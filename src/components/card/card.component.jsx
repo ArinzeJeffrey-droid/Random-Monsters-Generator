64479735261d44670ae2101b45e184038f11c2ce
arinzeJeffrey <nnamenearinze@gmail.com>
2020-07-21 23:32:05 +0100

    added the image

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ import "./card.styles.css"
 export const Card = (props) => {
     return (
         <div className="card-container">
-            <img src={`https://robohash.org/${prop.monster.id}?set=set2&180x180`} alt="monster"/>
+            <img src={`https://robohash.org/${props.monster.id}?set=set2&180x180`} alt="monster"/>
             <h1>{props.monster.name}</h1>
         </div>
     )
