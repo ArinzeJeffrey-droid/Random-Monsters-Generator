6d986be940c096c9bcc708d5eee2b0f63c4291ea
arinzeJeffrey <nnamenearinze@gmail.com>
2020-07-21 23:16:23 +0100

    broke the components down a little bit

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import {Card} from "../card/card.component"
 import "./card-list.style.css"
 
 export const CardList = (props) => {
-    return <div className="card-list">
-        {props.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)}
-    </div>
+    return (
+        <div className="card-list">
+            {props.monsters.map(monster => <Card key={monster.id} monster={monster}/>)}
+        </div>
+    )
 }
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Card = (props) => {
+    return (
+        <div>{props.monster.name}</div>
+    )
+}
