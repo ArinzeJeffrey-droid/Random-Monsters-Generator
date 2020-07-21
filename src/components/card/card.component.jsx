4f0f57618afeeddbac2f55dc745b7c8bb27199a9
arinzeJeffrey <nnamenearinze@gmail.com>
2020-07-21 23:34:08 +0100

    added the email

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,8 @@ export const Card = (props) => {
     return (
         <div className="card-container">
             <img src={`https://robohash.org/${props.monster.id}?set=set2&180x180`} alt="monster"/>
-            <h1>{props.monster.name}</h1>
+            <h2>{props.monster.name}</h2>
+            <p>{props.monster.email}</p>
         </div>
     )
 }
